dcd850c2a54136914c908f7e0a3ce0ed7334a6c5
Arefe Asadi <arefeasadi70@gmail.com>
2025-11-16 11:57:06 +0100

    delete gp

--- datalink.R
+++
@@ -1,7 +0,0 @@
-obsrp <- nc_open("C:/Users/arefe/Desktop/Personal Folder/My_Projects/downscaling/downscaling/obsrp/data_stream-moda_stepType-avgua.nc")
-
-datagp <- nc_open("C:/Users/arefe/Desktop/Personal Folder/My_Projects/downscaling/downscaling/datagp/sfcWind_Amon_IPSL-CM6A-LR_historical_r1i1p1f1_gr_19800116-19991216.nc")
-
-datagf <- nc_open("C:/Users/arefe/Desktop/Personal Folder/My_Projects/downscaling/downscaling/datagf/sfcWind_Amon_IPSL-CM6A-LR_historical_r1i1p1f1_gr_20000116-20141216.nc")
-
-obsval <- nc_open("C:/Users/arefe/Desktop/Personal Folder/My_Projects/downscaling/downscaling/obsval/data_stream-moda_stepType-avgua.nc")
